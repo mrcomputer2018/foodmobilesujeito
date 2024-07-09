f66db8eb5f1cb58877644cc7ab3bfabb39706b35
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-07-09 09:35:18 -0300

    chore: Update banner component with react-native-pager-view for image viewing

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,12 +1,21 @@
 import { View, Pressable, Text, Image } from 'react-native';
+import PagerView from 'react-native-pager-view';
 
 export function Banner() {
     return (
-        <View>
-            <Pressable>
-            <Image source={{ uri: 'https://via.placeholder.com/200' }} />
-            <Text>Click me</Text>
-            </Pressable>
+        <View 
+        className='bg-red-100 w-full h-36 rounded-2xl mt-5 mb-4'>
+            <PagerView 
+            style={{ flex: 1 }}
+            initialPage={0}
+            pageMargin={14}>
+                <Pressable 
+                className='w-full h-36 rounded-2xl'
+                key="1"
+                onPress={() => alert("clicou!!!")}>
+                    <Image source={{ uri: 'https://via.placeholder.com/200' }} />
+                </Pressable>
+            </PagerView>
         </View>    
     );
 }
