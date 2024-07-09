ef2d75df3cf788fb03a97b6408e470a75c89d9a1
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-07-09 09:15:11 -0300

    chore: Add lazy loading for images and update header component in index screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,27 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { Header } from "../components/header";
+import { Banner } from "../components/banner";
+
+import Constants from "expo-constants";
+
+
+const statusBarHeight = Constants.statusBarHeight; // pegando a altura da status bar
+
 export default function Index() {
     return (
-        <View className='w-full h-full flex-1'>
-            <Header />
-        </View>
+        <ScrollView 
+        className='w-full h-full flex-1 bg-slate-100'
+        showsVerticalScrollIndicator={false}
+        >
+            <View 
+            className="w-full px-4" 
+            style={{ marginTop: statusBarHeight }}>
+                <Header />
+            </View>
+
+            <View>
+                <Banner />
+            </View>
+        </ScrollView>
     );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,11 +3,27 @@ import { Ionicons, Feather } from "@expo/vector-icons"
 
 export function Header() {
     return (
-        <View>
-            <Pressable className='flex-row justify-between items-center p-3 bg-gray-800'>
-                <Ionicons name='menu' size={24} color='white' />
-                <Text className='text-white text-lg font-bold'>Home</Text>
-                <Feather name='search' size={24} color='white' />
+        <View className='w-full flex-row items-center justify-between'>
+            <Pressable 
+            className='w-10 h-10 bg-white rounded-full 
+            flex justify-center items-center'>
+                <Ionicons name='menu' size={20} color='#121212' />
+            </Pressable>
+
+            <View className='flex flex-col justify-center items-center gap-1'>
+                <Text className='text-sm text-center text-slate-700'>
+                    Localização
+                </Text>
+
+                <View className='flex flex-row justify-center items-center gap-1'>
+                    <Feather name='map-pin' size={14} color='#ff0000' />
+                    <Text className='text-lg font-bold'>Campo Grande</Text>
+                </View>
+            </View>
+
+            <Pressable className='w-10 h-10 bg-white rounded-full 
+            flex justify-center items-center'>        
+                <Feather name='bell' size={20} color='#121212' />
             </Pressable>
         </View>
     );
