fb0613f3bea7361f3cb13fbaddfbe71eb0dedd34
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-07-09 09:18:29 -0300

    chore: Add react-native-pager-view dependency and update banner component

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,7 +1,12 @@
-import { View } from 'react-native';
+import { View, Pressable, Text, Image } from 'react-native';
 
 export function Banner() {
- return (
-   <View/>
-  );
+    return (
+        <View>
+            <Pressable>
+            <Image source={{ uri: 'https://via.placeholder.com/200' }} />
+            <Text>Click me</Text>
+            </Pressable>
+        </View>    
+    );
 }
